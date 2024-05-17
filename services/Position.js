0b467bf63f868858d8fdec05425144624dce3db8
Mark Parker <marksadpepe@gmail.com>
2024-05-18 00:48:17 +0300

    added method to find position by ID

--- services/Position.js
+++ services/Position.js
@@ -15,7 +15,26 @@ class Position {
       return positionsData;
     } catch (err) {
       if (err.message.includes("404")) {
-        throw new Error("404:Positions not found");
+        throw new Error(err.message);
+      }
+      console.error(err.message);
+      throw new Error("500:Internal Server Error");
+    }
+  }
+
+  async getPositionById(positionId) {
+    try {
+      const position = await PositionModel.findOne({
+        where: {id: positionId}
+      });
+      if (!position) {
+        throw new Error("404:Position not found");
+      }
+
+      return new PositionDto(position);
+    } catch (err) {
+      if (err.message.includes("404")) {
+        throw new Error(err.message);
       }
       console.error(err.message);
       throw new Error("500:Internal Server Error");
