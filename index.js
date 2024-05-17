bdf85215a4ae0fa3d5ca30f38b6b277129a75db1
Mark Parker <marksadpepe@gmail.com>
2024-05-17 14:01:11 +0300

    init basic start app file

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const sql = require("./db.js");
+const models = require("./models/relations.js");
+
+const startApp = async() => {
+  try {
+    await sql.authenticate();
+  } catch (err) {
+    console.error("Unable to connect to the database: ", err);
+  }
+
+  try {
+    await sql.sync();
+  } catch (err) {
+    console.error("Failed to sync database: ", err);
+  }
+};
+
+startApp();
