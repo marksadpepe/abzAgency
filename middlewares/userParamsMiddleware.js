de86239cdaeb39976717bc054d9585fa82e361ab
Mark Parker <marksadpepe@gmail.com>
2024-05-19 20:25:50 +0300

    added middleware to check the given params with request

---
+++ middlewares/userParamsMiddleware.js
@@ -0,0 +1,24 @@
+module.exports = function(req, res, next) {
+  if (!req.params || !req.params.id) {
+    return res.status(400).json({
+      success: false,
+      message: "The user ID must be non-empty"
+    });
+  }
+
+  const userId = req.params.id;
+  if (!Number(userId)) {
+    return res.status(400).json({
+      success: false,
+      message: "The user with the requestedid does not exist",
+      fails: {
+        userId: [
+          "The user must be an integer"
+        ]
+      }
+    });
+  }
+  req.params.id = Number(userId);
+
+  next();
+}
