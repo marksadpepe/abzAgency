335b844666c1ed0f01f2ed05b5bb766f7c4ac38d
Mark Parker <marksadpepe@gmail.com>
2024-05-20 14:41:38 +0300

    added service to compress and resize users profile pics

--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,7 @@ const UserController = require("../controllers/User.js");
 const FileService = require("../services/File.js");
 const tokenMdw = require("../middlewares/tokenMiddleware.js");
 const fileUploadMwd = require("../middlewares/fileUploadMiddleware.js");
-const fileResMdw = require("../middlewares/fileResolutionMiddleware.js");
+const fileTinifyMdw = require("../middlewares/fileTinifyMiddleware.js");
 const userParamsMdw = require("../middlewares/userParamsMiddleware.js");
 const paginationMdw = require("../middlewares/paginationMiddleware.js");
 const bodyPayloadMdw = require("../middlewares/bodyPayloadMiddleware.js");
@@ -13,6 +13,6 @@ const upload = FileService.getUpload();
 
 router.get("/users", paginationMdw, UserController.getUsers);
 router.get("/users/:id", userParamsMdw, UserController.getUser);
-router.post("/users", tokenMdw, upload.single("profilePic"), fileUploadMwd, fileResMdw, bodyPayloadMdw, UserController.registerUser);
+router.post("/users", tokenMdw, upload.single("profilePic"), fileUploadMwd, fileTinifyMdw, bodyPayloadMdw, UserController.registerUser);
 
 module.exports = router;
