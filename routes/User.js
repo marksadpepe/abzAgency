efe474c41933c663898d4b34f895c80964909bcf
Mark Parker <marksadpepe@gmail.com>
2024-05-20 13:34:14 +0300

    added middleware for the requested body

--- routes/User.js
+++ routes/User.js
@@ -6,12 +6,13 @@ const fileUploadMwd = require("../middlewares/fileUploadMiddleware.js");
 const fileResMdw = require("../middlewares/fileResolutionMiddleware.js");
 const userParamsMdw = require("../middlewares/userParamsMiddleware.js");
 const paginationMdw = require("../middlewares/paginationMiddleware.js");
+const bodyPayloadMdw = require("../middlewares/bodyPayloadMiddleware.js");
 
 const router = express.Router();
 const upload = FileService.getUpload();
 
 router.get("/users", paginationMdw, UserController.getUsers);
 router.get("/users/:id", userParamsMdw, UserController.getUser);
-router.post("/users", tokenMdw, upload.single("profilePic"), fileUploadMwd, fileResMdw, UserController.registerUser);
+router.post("/users", tokenMdw, upload.single("profilePic"), fileUploadMwd, fileResMdw, bodyPayloadMdw, UserController.registerUser);
 
 module.exports = router;
